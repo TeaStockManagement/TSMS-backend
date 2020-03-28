7122382418d9471b61e95b3bbc328a0dc097559b
UKALHMA <malith.kalhara@pearson.com>
2020-03-28 20:11:42 +0530

    Added user registration successfully

--- models/user.model.js
+++ models/user.model.js
@@ -3,6 +3,11 @@ var mongoose = require('mongoose');
 var userSchema=mongoose.Schema({
     username: String,
     password:String,
+    fname:String,
+    lname:String,
+    mobile:Number,
+    email:String,
+    nic:String
 });
 
 var User = mongoose.model('User', userSchema);
